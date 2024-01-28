403806c7d7835d11b8f7d0d58a8da55b99bd67e5
YYashSingh-9 <yyashsngh@gmail.com>
2024-01-28 13:53:05 -0800

    login/signup done

--- Backend/Router/UserRouter.js
+++ Backend/Router/UserRouter.js
@@ -4,5 +4,6 @@ const userController = require("../Controllers/UserController");
 const userRouter = express.Router();
 
 userRouter.route("/signup").post(userController.signupUser);
+userRouter.route("/login").post(userController.loginUser);
 
 module.exports = userRouter;
